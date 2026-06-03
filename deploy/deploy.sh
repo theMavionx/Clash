c0aef91656842c0e028d337131575c97838fd92b
egorble <egor4042007@gmail.com>
2026-06-03 19:20:18 +0300

    Keep Hotstuff env out of deploy script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -281,15 +281,6 @@ prepare_shared_runtime() {
                 printf '%s\n' VITE_GRVT_BUILDER_ACCOUNT_ID=0x5d2c800b71f71fcf6bbf990f25ec39de09a87b45
                 printf '%s\n' VITE_GRVT_BUILDER_FEE_BPS=5
                 printf '%s\n' VITE_GRVT_BUILDER_FEE_RATE=0.0005
-                printf '%s\n' HOTSTUFF_BROKER_ADDRESS=0xB36402e87a86206D3a114a98B53f31362291fe1B
-                printf '%s\n' HOTSTUFF_AGENT_ADDRESS=0xd186C1F7a6ad2A6380fd019978C299DB4cFe5589
-                printf '%s\n' HOTSTUFF_API_WALLET_PRIVATE_KEY=
-                printf '%s\n' HOTSTUFF_BROKER_FEE_RATE=0.0005
-                printf '%s\n' HOTSTUFF_BROKER_MIN_ACCOUNT_VALUE_USDC=100
-                printf '%s\n' VITE_HOTSTUFF_BROKER_ADDRESS=0xB36402e87a86206D3a114a98B53f31362291fe1B
-                printf '%s\n' VITE_HOTSTUFF_BROKER_FEE_RATE=0.0005
-                printf '%s\n' VITE_HOTSTUFF_REFERRAL_CODE=clashofperps
-                printf '%s\n' VITE_HOTSTUFF_REFERRAL_URL=https://app.hotstuff.trade/join/clashofperps
                 printf '%s=%s\n' CLASH_WALLET_ENCRYPTION_KEY "$generated_wallet_enc"
             } > "$ENV_FILE"
             chmod 600 "$ENV_FILE"
@@ -322,15 +313,6 @@ prepare_shared_runtime() {
     ensure_env_default "VITE_GRVT_BUILDER_ACCOUNT_ID" "0x5d2c800b71f71fcf6bbf990f25ec39de09a87b45"
     ensure_env_default "VITE_GRVT_BUILDER_FEE_BPS" "5"
     ensure_env_default "VITE_GRVT_BUILDER_FEE_RATE" "0.0005"
-    ensure_env_default "HOTSTUFF_BROKER_ADDRESS" "0xB36402e87a86206D3a114a98B53f31362291fe1B"
-    ensure_env_default "HOTSTUFF_AGENT_ADDRESS" "0xd186C1F7a6ad2A6380fd019978C299DB4cFe5589"
-    ensure_env_default "HOTSTUFF_API_WALLET_PRIVATE_KEY" ""
-    ensure_env_default "HOTSTUFF_BROKER_FEE_RATE" "0.0005"
-    ensure_env_default "HOTSTUFF_BROKER_MIN_ACCOUNT_VALUE_USDC" "100"
-    ensure_env_default "VITE_HOTSTUFF_BROKER_ADDRESS" "0xB36402e87a86206D3a114a98B53f31362291fe1B"
-    ensure_env_default "VITE_HOTSTUFF_BROKER_FEE_RATE" "0.0005"
-    ensure_env_default "VITE_HOTSTUFF_REFERRAL_CODE" "clashofperps"
-    ensure_env_default "VITE_HOTSTUFF_REFERRAL_URL" "https://app.hotstuff.trade/join/clashofperps"
     ensure_env_default "CLASH_WALLET_ENCRYPTION_KEY" "$(openssl rand -hex 32)"
     ensure_env_default "VITE_PRIVY_APP_ID" ""
     ensure_env_default "VITE_APTOS_NODE_API_KEY" ""
@@ -393,14 +375,6 @@ prepare_shared_runtime() {
     set_env_value "VITE_GRVT_BUILDER_ACCOUNT_ID" "0x5d2c800b71f71fcf6bbf990f25ec39de09a87b45"
     set_env_value "VITE_GRVT_BUILDER_FEE_BPS" "5"
     set_env_value "VITE_GRVT_BUILDER_FEE_RATE" "0.0005"
-    set_env_value "HOTSTUFF_BROKER_ADDRESS" "0xB36402e87a86206D3a114a98B53f31362291fe1B"
-    set_env_value "HOTSTUFF_AGENT_ADDRESS" "0xd186C1F7a6ad2A6380fd019978C299DB4cFe5589"
-    set_env_value "HOTSTUFF_BROKER_FEE_RATE" "0.0005"
-    set_env_value "HOTSTUFF_BROKER_MIN_ACCOUNT_VALUE_USDC" "100"
-    set_env_value "VITE_HOTSTUFF_BROKER_ADDRESS" "0xB36402e87a86206D3a114a98B53f31362291fe1B"
-    set_env_value "VITE_HOTSTUFF_BROKER_FEE_RATE" "0.0005"
-    set_env_value "VITE_HOTSTUFF_REFERRAL_CODE" "clashofperps"
-    set_env_value "VITE_HOTSTUFF_REFERRAL_URL" "https://app.hotstuff.trade/join/clashofperps"
     set_env_value "CLASH_MAIN_DB" "$SHARED_SERVER_DIR/clash.db"
     set_env_value "CLASH_FUTURES_DB" "$SHARED_FUTURES_DIR/futures.db"
     set_env_value "CLASH_MCP_PUBLIC_URL" "https://$MCP_DOMAIN"
